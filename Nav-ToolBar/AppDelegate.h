8a80f061afce964d88621b17e4185d58ea5b3c62
wangtao <umpay@wangtao.local>
2014-05-28 12:57:01 +0800

    测试UINavigationController中集成的UIToolBar控件
    首次初始项目

---
+++ Nav-ToolBar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Nav-ToolBar
+//
+//  Created by wangtao on 14-5-28.
+//  Copyright (c) 2014年 wangtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
